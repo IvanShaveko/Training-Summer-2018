af2e926fcc43057e23f1048f6fa1817c2936c1c8
IvanShaveko <shaveko.iva@gmail.com>
2018-07-06 00:12:47 +0300

    not full

---
+++ NET.S.2018.Shaveko.04/DoubleExtension.Tests/DoubleExtensionTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using DoubleExtension;
+
+namespace DoubleExtension.Tests
+{
+    [TestFixture]
+    public class DoubleExtensionTests
+    {
+        [TestCase(255.255, ExpectedResult = "0100000001101111111010000010100011110101110000101000111101011100")]
+        [TestCase(4294967295.0, ExpectedResult = "0100000111101111111111111111111111111111111000000000000000000000")]
+        [TestCase(-255.255, ExpectedResult = "1100000001101111111010000010100011110101110000101000111101011100")]
+
+        [Test]
+        public string ConvertToBit_Double_Success(double number)
+        {
+            return number.ConvertToBit();
+        }
+
+    }
+}
---
+++ NET.S.2018.Shaveko.04/DoubleExtension/DoubleExtension.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleExtension
+{
+    /// <summary>
+    /// Convert to IEEE
+    /// </summary>
+    public static class DoubleExtension
+    {
+        /// <summary>
+        /// Method to convert ro IEEE
+        /// </summary>
+        /// <param name="number">
+        /// The number
+        /// </param>
+        /// <returns>
+        /// Return IEEE
+        /// </returns>
+        public static string ConvertToBit(this double number)
+        {
+            string s = "";
+
+            s = string.Concat(s, ConvertToBitWholePart(Math.Abs(Math.Truncate(number)), out int index));
+            s = string.Concat(s, ConvertToBitFractionalPart(Math.Abs(number) - Math.Abs(Math.Truncate(number))));
+
+            s = ConvertToIeee(s, index);
+
+            return number.AddSign(s);
+        }
+
+        /// <summary>
+        /// Convert whole part to binary code
+        /// </summary>
+        /// <param name="number">
+        /// The number 
+        /// </param>
+        /// <param name="i">
+        /// Count of bit
+        /// </param>
+        /// <returns>
+        /// The string of binary code
+        /// </returns>
+        private static string ConvertToBitWholePart(this double number, out int i)
+        {
+            i = 0;
+            string str = "";
+            while (number > 0)
+            {
+                str = String.Concat(Convert.ToString(number % 2), str);
+                number = Math.Truncate(number / 2);
+                i++;
+            }
+
+            return str;
+        }
+
+        /// <summary>
+        /// Convert fraction part to bit
+        /// </summary>
+        /// <param name="fraction">
+        /// The fraction
+        /// </param>
+        /// <returns>
+        /// String of fraction bits
+        /// </returns>
+        private static string ConvertToBitFractionalPart(this double fraction)
+        {
+            string list = "";
+            while (list.Length < 52)
+            {
+                fraction *= 2;
+                long c = Convert.ToInt64(Math.Truncate(fraction));
+                list = string.Concat(list, Convert.ToString(c));
+                fraction -= c;
+            }
+
+            char[] tmp = list.ToCharArray();
+            int i = 0;
+
+            while (tmp[tmp.Length - 1 - i] == '0' && i != tmp.Length - 1)
+            {
+                i++;
+            }
+
+            list = list.Remove(tmp.Length - i);
+
+            return list;
+        }
+
+        /// <summary>
+        /// Convert bits to IEEE
+        /// </summary>
+        /// <param name="s">
+        /// The string of bits
+        /// </param>
+        /// <param name="index">
+        /// The index
+        /// </param>
+        /// <returns>
+        /// Returns IEEE code
+        /// </returns>
+        private static string ConvertToIeee(string s, int index)
+        {
+            double order = 1023 + index - 1;
+            string str = ConvertToBitWholePart(order, out _);
+            while (str.Length < 11)
+            {
+                str = string.Concat(str, "0");
+            }
+
+            str = string.Concat(str, s.Remove(0, 1));
+
+            while (str.Length < 63)
+            {
+                str = string.Concat(str, "0");
+            }
+
+            return str;
+        }
+
+        /// <summary>
+        /// Add sign to IEEE code
+        /// </summary>
+        /// <param name="number">
+        /// The number
+        /// </param>
+        /// <param name="s">
+        /// The string 
+        /// </param>
+        /// <returns>
+        /// IEEE code with sign
+        /// </returns>
+        private static string AddSign(this double number, string s)
+        {
+            if (number > 0)
+            {
+                s = string.Concat("0", s);
+            }
+            else
+            {
+                s = string.Concat("1", s);
+            }
+
+            return s;
+        }
+    }
+}

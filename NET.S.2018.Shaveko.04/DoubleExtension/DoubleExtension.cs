9b8328d548b00f808d3e0b156847a46516e44fc1
IvanShaveko <shaveko.iva@gmail.com>
2018-07-07 21:01:28 +0300

    Fixes

--- NET.S.2018.Shaveko.04/DoubleExtension/DoubleExtension.cs
+++ NET.S.2018.Shaveko.04/DoubleExtension/DoubleExtension.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,141 +12,49 @@ namespace DoubleExtension
     /// </summary>
     public static class DoubleExtension
     {
-        /// <summary>
-        /// Method to convert ro IEEE
-        /// </summary>
-        /// <param name="number">
-        /// The number
-        /// </param>
-        /// <returns>
-        /// Return IEEE
-        /// </returns>
-        public static string ConvertToBit(this double number)
+        private const int BITS = 64;
+
+        public static string DoubleToBinary(this double number)
         {
-            string s = "";
+            DoubleToLongStruct convert = new DoubleToLongStruct{Double64Bits = number};
+            long long64Bits = convert.Long64Bits();
+            string stringBits = "";
 
-            s = string.Concat(s, ConvertToBitWholePart(Math.Abs(Math.Truncate(number)), out int index));
-            s = string.Concat(s, ConvertToBitFractionalPart(Math.Abs(number) - Math.Abs(Math.Truncate(number))));
+            for (int i = 0; i < BITS; i++)
+            {
+                char bit;
+                if ((long64Bits & 1) == 1)
+                {
+                    bit = '1';
+                }
+                else
+                {
+                    bit = '0';
+                }
 
-            s = ConvertToIeee(s, index);
+                stringBits = bit + stringBits;
 
-            return number.AddSign(s);
+                long64Bits >>= 1;
+            }
+
+            return stringBits;
         }
 
-        /// <summary>
-        /// Convert whole part to binary code
-        /// </summary>
-        /// <param name="number">
-        /// The number 
-        /// </param>
-        /// <param name="i">
-        /// Count of bit
-        /// </param>
-        /// <returns>
-        /// The string of binary code
-        /// </returns>
-        private static string ConvertToBitWholePart(this double number, out int i)
+        [StructLayout(LayoutKind.Explicit)]
+        private struct DoubleToLongStruct
         {
-            i = 0;
-            string str = "";
-            while (number > 0)
+            [FieldOffset(0)]
+            private readonly long long64bits;
+
+            [FieldOffset(0)]
+            private  double double64bits;
+
+            public double Double64Bits
             {
-                str = String.Concat(Convert.ToString(number % 2), str);
-                number = Math.Truncate(number / 2);
-                i++;
+                set => double64bits = value;
             }
 
-            return str;
-        }
-
-        /// <summary>
-        /// Convert fraction part to bit
-        /// </summary>
-        /// <param name="fraction">
-        /// The fraction
-        /// </param>
-        /// <returns>
-        /// String of fraction bits
-        /// </returns>
-        private static string ConvertToBitFractionalPart(this double fraction)
-        {
-            string list = "";
-            while (list.Length < 52)
-            {
-                fraction *= 2;
-                long c = Convert.ToInt64(Math.Truncate(fraction));
-                list = string.Concat(list, Convert.ToString(c));
-                fraction -= c;
-            }
-
-            char[] tmp = list.ToCharArray();
-            int i = 0;
-
-            while (tmp[tmp.Length - 1 - i] == '0' && i != tmp.Length - 1)
-            {
-                i++;
-            }
-
-            list = list.Remove(tmp.Length - i);
-
-            return list;
-        }
-
-        /// <summary>
-        /// Convert bits to IEEE
-        /// </summary>
-        /// <param name="s">
-        /// The string of bits
-        /// </param>
-        /// <param name="index">
-        /// The index
-        /// </param>
-        /// <returns>
-        /// Returns IEEE code
-        /// </returns>
-        private static string ConvertToIeee(string s, int index)
-        {
-            double order = 1023 + index - 1;
-            string str = ConvertToBitWholePart(order, out _);
-            while (str.Length < 11)
-            {
-                str = string.Concat(str, "0");
-            }
-
-            str = string.Concat(str, s.Remove(0, 1));
-
-            while (str.Length < 63)
-            {
-                str = string.Concat(str, "0");
-            }
-
-            return str;
-        }
-
-        /// <summary>
-        /// Add sign to IEEE code
-        /// </summary>
-        /// <param name="number">
-        /// The number
-        /// </param>
-        /// <param name="s">
-        /// The string 
-        /// </param>
-        /// <returns>
-        /// IEEE code with sign
-        /// </returns>
-        private static string AddSign(this double number, string s)
-        {
-            if (number > 0)
-            {
-                s = string.Concat("0", s);
-            }
-            else
-            {
-                s = string.Concat("1", s);
-            }
-
-            return s;
+            public long Long64Bits() => long64bits;
         }
     }
 }
